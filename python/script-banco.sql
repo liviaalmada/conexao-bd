ffbd7e6c0e400dc425b36240830583c710539ebb
Lívia Almada <almada.livia@gmail.com>
2024-09-04 07:27:39 -0300

    Update script-banco.sql

--- python/script-banco.sql
+++ python/script-banco.sql
@@ -1,24 +1,14 @@
 
--- Table: public.pessoa
-
--- DROP TABLE public.pessoa;
 
 CREATE TABLE public.pessoa
 (
-idp integer NOT NULL DEFAULT nextval('pessoa_idp_seq'::regclass),
-nome character varying(50) COLLATE pg_catalog."default",
-cpf character varying(12) COLLATE pg_catalog."default" NOT NULL,
-sexo character varying(12) COLLATE pg_catalog."default",
+idp serial,
+nome character varying(50),
+cpf character varying(12),
+sexo character varying(12),
 nascimento date,
-salario double precision,
+salario numeric(1000,2),
 CONSTRAINT pessoa_pkey PRIMARY KEY (cpf),
 CONSTRAINT id_unique UNIQUE (idp)
 INCLUDE(idp)
-)
-WITH (
-OIDS = FALSE
-)
-TABLESPACE pg_default;
-
-ALTER TABLE public.pessoa
-OWNER to postgres;
+);
